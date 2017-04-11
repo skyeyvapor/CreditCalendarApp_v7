563a06ab0ba973d38d5ca8fa0d4a437bda131874
skyeyvapor <skyeyvapor@gmail.com>
2017-04-10 22:54:54 -0700

    Initial Commit

---
+++ CreditCalendarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreditCalendarApp
+//
+//  Created by Ching-Lan Chen on 2017/4/10.
+//  Copyright © 2017年 Credit Calendar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
